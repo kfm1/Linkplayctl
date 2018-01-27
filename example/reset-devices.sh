3cb03c1848ccf3fb005664002efaf814bf311490
kfm1 <kurt@kurtm.net>
2018-01-27 11:35:02 -0800

    clean up example script

--- example/reset-devices.sh
+++ example/reset-devices.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
-# Reboot and reset volume for a list of linkplay devices
+# Reboot and reset volume and voice prompts for a list of linkplay devices
 #
 # Linkplay devices get unstable after being up for too long.  This script takes a list of devices and
-# performs the following actions on each device:
+# performs the following actions on each one:
 #   * Quiet reboot
 #   * Disable voice prompts and chimes
 #   * Set volume to a default value
-#
+#   * TODO: Set muting to a default value
 
 # List of all the linkplay devices that should be reset:
 DEVICES=("192.168.2.81" "192.168.2.82" "192.168.2.83" "192.168.2.84")
 
-# Volume that each device should be set to after reboot.  Comment out to leave volume unchanged.
+# Volume that each device should be set to after reboot, comment out to leave volume unchanged:
 DEFAULT_VOLUME=50
-
-# Path to linkplayctl bash script
-#   Default: ../bin/linkplayctl
-#LINKPLAYCTL="../bin/linkplayctl"
-
 
 printf "\n\nStarting linkplayctl device reset script...\n"
 
@@ -53,12 +48,7 @@ do
     sleep 2
 
     # Reboot device quietly
-    ${LINKPLAYCTL} -vvv ${device} quiet-reboot
-    printf '\n'
-    sleep 2
-
-    # Print current volume setting
-    ${LINKPLAYCTL} -v ${device} volume
+    ${LINKPLAYCTL} -v ${device} quiet-reboot
     printf '\n'
     sleep 2
 
@@ -67,7 +57,7 @@ do
     printf '\n'
     sleep 2
 
-    # If new default volume, the set device to new volume
+    # Set device to new volume, if provided
     if [[ ! -z "${DEFAULT_VOLUME}" ]]
     then
         ${LINKPLAYCTL} -v ${device} volume ${DEFAULT_VOLUME}
